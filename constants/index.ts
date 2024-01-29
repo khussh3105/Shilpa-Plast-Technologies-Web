1624f99dfa5c35002cf3d7099d24cef533c351e9
khussh3105 <99252331+khussh3105@users.noreply.github.com>
2024-01-29 16:41:07 +0530

    Add files via upload

---
+++ constants/index.ts
@@ -0,0 +1,36 @@
+// Navbar
+export const NAV_LINKS = [
+    { href: '/Hero', key: 'home', label: 'Home' },
+    { href: '/About', key: 'about', label: 'About Us' },
+    { href: '/Machines', key: 'services', label: 'Services' },
+    { href: '/Contact', key: 'contact', label: 'Contact Us' },
+  ];
+  
+  
+  // Machines
+  export const FEATURES = [
+    {
+      title: 'Scrap Collection',
+      icon: '/android.svg',
+      description:
+        "Scrap collection is a crucial step in the plastic recycling process, as it involves gathering used or discarded plastic materials to be processed and transformed into Plastic Granules. ",
+    },
+    {
+      title: 'Raw Material Pre-Processing',
+      icon: '/calendar.svg',
+      description:
+        "Raw material pre-processing is a critical stage in a plastic recycling unit where collected plastic scrap undergoes cleaning, sorting, and sometimes transforming the scrap into a more manageable form further processing. ",
+    },
+    {
+      title: 'Conversion Into Plastic Granules',
+      icon: '/tech.svg',
+      description:
+      "The conversion of pre-processed plastic material into plastic granules is a key step which involves melting, shaping, and cooling the plastic material into small, uniform granules. Plastic granules serve as the raw material for manufacturing a variety of plastic products. ",
+    },
+    {
+      title: 'Delivery to Customers',
+      icon: '/location.svg',
+      description:
+        "The delivery of recycled plastic products to customers is a crucial aspect of the plastic recycling unit's operations. It involves logistics, transportation, and coordination to ensure that the recycled plastic materials reach their intended destination in a timely and efficient manner.",
+    },
+  ];
